24305cd1ac324c99857949f835e95710b96126fc
unknown <arijitg508@gmail.com>
2023-08-03 00:22:23 +0530

    update contact.

--- src/Content.js
+++ src/Content.js
@@ -1,34 +1,25 @@
 // import images
 import Hero_person from "./assets/images/Hero/person.png";
-
-import figma from "./assets/images/Skills/figma.png";
-import sketch from "./assets/images/Skills/sketch.png";
-import ps from "./assets/images/Skills/ps.png";
-import reactjs from "./assets/images/Skills/react.png";
-import nodejs from "./assets/images/Skills/node.png";
-import python from "./assets/images/Skills/python.png";
 import stopwatch from './assets/images/stopwatch.jpg'
-import breakfast from './assets/images/breakfast.jpg'
 import flag from './assets/images/flag.png'
 import run from './assets/images/run10.jpg'
 import food from './assets/images/food.png'
 
 import services_logo1 from "./assets/images/Services/logo1.png";
 import services_logo2 from "./assets/images/Services/logo2.png";
-import services_logo3 from "./assets/images/Services/logo3.png";
 
-import project1 from "./assets/images/projects/img1.png";
-import project2 from "./assets/images/projects/img2.png";
-import project3 from "./assets/images/projects/img3.png";
-import person_project from "./assets/images/projects/person.png";
+// import project1 from "./assets/images/projects/img1.png";
+// import project2 from "./assets/images/projects/img2.png";
+// import project3 from "./assets/images/projects/img3.png";
+// import person_project from "./assets/images/projects/person.png";
 
-import avatar1 from "./assets/images/Testimonials/avatar1.png";
-import avatar2 from "./assets/images/Testimonials/avatar2.png";
-import avatar3 from "./assets/images/Testimonials/avatar3.png";
-import avatar4 from "./assets/images/Testimonials/avatar4.png";
+// import avatar1 from "./assets/images/Testimonials/avatar1.png";
+// import avatar2 from "./assets/images/Testimonials/avatar2.png";
+// import avatar3 from "./assets/images/Testimonials/avatar3.png";
+// import avatar4 from "./assets/images/Testimonials/avatar4.png";
 
-import Hireme_person from "./assets/images/Hireme/person.png";
-import Hireme_person2 from "./assets/images/Hireme/person2.png";
+// import Hireme_person from "./assets/images/Hireme/person.png";
+// import Hireme_person2 from "./assets/images/Hireme/person2.png";
 
 // import icons from react-icons
 import { GrMail } from "react-icons/gr";
@@ -141,59 +132,59 @@ export const content = {
   Projects: {
     title: "Starting Point",
     subtitle: "Ready, Set, Go!",
-    image: person_project,
+    // image: person_project,
     project_content: [
       {
         title: "Gym Website",
-        image: project1,
+        // image: project1,
       },
       {
         title: "Social Media web",
-        image: project2,
+        // image: project2,
       },
       {
         title: "Creative Website",
-        image: project3,
+        // image: project3,
       },
     ],
   },
   Testimonials: {
-    title: "Countdown",
-    subtitle: "SEE YOU SOON!",
-    testimonials_content: [
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar1,
-        name: "JOHN DOE",
-      },
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar2,
-        name: "Tom Alex",
-      },
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar3,
-        name: "Johnny",
-      },
-      {
-        review:
-          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
-        img: avatar4,
-        name: "ROBBIN",
-      },
-    ],
+    // title: "Countdown",
+    // subtitle: "SEE YOU SOON!",
+    // testimonials_content: [
+    //   {
+    //     review:
+    //       "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+    //     img: avatar1,
+    //     name: "JOHN DOE",
+    //   },
+    //   {
+    //     review:
+    //       "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+    //     img: avatar2,
+    //     name: "Tom Alex",
+    //   },
+    //   {
+    //     review:
+    //       "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+    //     img: avatar3,
+    //     name: "Johnny",
+    //   },
+    //   {
+    //     review:
+    //       "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+    //     img: avatar4,
+    //     name: "ROBBIN",
+    //   },
+    // ],
   },
   Hireme: {
-    title: "Hire Me",
-    subtitle: "FOR YOUR PROJECTS",
-    image1: Hireme_person,
-    image2: Hireme_person2,
-    para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
-    btnText: "Hire Me",
+    // title: "Hire Me",
+    // subtitle: "FOR YOUR PROJECTS",
+    // image1: Hireme_person,
+    // image2: Hireme_person2,
+    // para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+    // btnText: "Hire Me",
   },
   Contact: {
     title: "Contact",
