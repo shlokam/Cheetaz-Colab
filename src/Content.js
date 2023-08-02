faf9ba4151746a2153ecffc736dbcf11c7eca6b3
unknown <arijitg508@gmail.com>
2023-08-02 23:21:26 +0530

    developed.

---
+++ src/Content.js
@@ -0,0 +1,250 @@
+// import images
+import Hero_person from "./assets/images/Hero/person.png";
+
+import figma from "./assets/images/Skills/figma.png";
+import sketch from "./assets/images/Skills/sketch.png";
+import ps from "./assets/images/Skills/ps.png";
+import reactjs from "./assets/images/Skills/react.png";
+import nodejs from "./assets/images/Skills/node.png";
+import python from "./assets/images/Skills/python.png";
+import stopwatch from './assets/images/stopwatch.jpg'
+import breakfast from './assets/images/breakfast.jpg'
+import flag from './assets/images/flag.png'
+import run from './assets/images/run10.jpg'
+import food from './assets/images/food.png'
+
+import services_logo1 from "./assets/images/Services/logo1.png";
+import services_logo2 from "./assets/images/Services/logo2.png";
+import services_logo3 from "./assets/images/Services/logo3.png";
+
+import project1 from "./assets/images/projects/img1.png";
+import project2 from "./assets/images/projects/img2.png";
+import project3 from "./assets/images/projects/img3.png";
+import person_project from "./assets/images/projects/person.png";
+
+import avatar1 from "./assets/images/Testimonials/avatar1.png";
+import avatar2 from "./assets/images/Testimonials/avatar2.png";
+import avatar3 from "./assets/images/Testimonials/avatar3.png";
+import avatar4 from "./assets/images/Testimonials/avatar4.png";
+
+import Hireme_person from "./assets/images/Hireme/person.png";
+import Hireme_person2 from "./assets/images/Hireme/person2.png";
+
+// import icons from react-icons
+import { GrMail } from "react-icons/gr";
+import { MdArrowForward, MdCall } from "react-icons/md";
+import { BsInstagram } from "react-icons/bs";
+import { TbSmartHome } from "react-icons/tb";
+import { BiUser } from "react-icons/bi";
+import { RiServiceLine, RiProjectorLine } from "react-icons/ri";
+import { MdOutlinePermContactCalendar } from "react-icons/md";
+import {BsFillPersonFill} from "react-icons/bs";
+
+export const content = {
+  nav: [
+    {
+      link: "#home",
+      icon: TbSmartHome,
+    },
+    {
+      link: "#skills",
+      icon: BiUser,
+    },
+    {
+      link: "#services",
+      icon: RiServiceLine,
+    },
+    {
+      link: "#projects",
+      icon: RiProjectorLine,
+    },
+    {
+      link: "#contact",
+      icon: MdOutlinePermContactCalendar,
+    },
+  ],
+  hero: {
+    title: "Collaboration Run",
+    firstName: "Chhetaz",
+    LastName: "Running Squad",
+    btnText: "Enroll Now",
+    image: Hero_person,
+    hero_content: [
+      {
+        count: "06 August 2023",
+        text: "Sunday, 5.45 AM Onwards",
+      },
+      // {
+      //   count: "20+",
+      //   text: "Projects Worked in my career",
+      // },
+    ],
+  },
+  skills: {
+    title: "Schedule",
+    subtitle: "Program Flow",
+    skills_content: [
+      {
+        name: "Meeting Time",
+        para: "05:15AM Onwards",
+        logo: stopwatch,
+      },
+      {
+        name: "National Anthem",
+        para: "05:30AM - 05:45AM ",
+        logo: flag,
+      },
+      {
+        name: "Start RUN",
+        para: "05:45AM Onwards",
+        logo: run,
+      },
+      {
+        name: "Breakfast",
+        para: "07:00AM Onwards",
+        logo: food,
+      },
+      // {
+      //   name: "Sketch",
+      //   para: "Lorem ipsum text  dummy",
+      //   logo: sketch,
+      // },
+      // {
+      //   name: "Python",
+      //   para: "Lorem ipsum text  dummy",
+      //   logo: python,
+      // },
+    ],
+    icon: MdArrowForward,
+  },
+  services: {
+    title: "Tracks",
+    subtitle: "OPTIONS PROVIDED",
+    service_content: [
+      {
+        title: "5KM",
+        para: "So, let's make today legendary! Embrace the challenge, embrace the thrill, and embrace the feeling of absolute accomplishment as you cross that 5 km mark. You've got this! Believe in yourself and show the world what you're made of! Go out there and make it happen!",
+                logo: services_logo1,
+      },
+      {
+        title: "10KM",
+        para: "So, let the adrenaline flow, let the joy of running consume you, and let your heart guide you through those 10 km. You're unstoppable, and nothing can stand in your way. Go out there and conquer this challenge with a smile on your face and fire in your soul. The world is cheering you on, and today, you're shining brighter than ever before! Run like the wind, and let the magic happen! You've got this!",        
+        logo: services_logo2,
+      },
+      // {
+      //   title: "PhotoShop Editing",
+      //   para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+      //   logo: services_logo3,
+      // },
+    ],
+  },
+  Projects: {
+    title: "Starting Point",
+    subtitle: "Ready, Set, Go!",
+    image: person_project,
+    project_content: [
+      {
+        title: "Gym Website",
+        image: project1,
+      },
+      {
+        title: "Social Media web",
+        image: project2,
+      },
+      {
+        title: "Creative Website",
+        image: project3,
+      },
+    ],
+  },
+  Testimonials: {
+    title: "Countdown",
+    subtitle: "SEE YOU SOON!",
+    testimonials_content: [
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar1,
+        name: "JOHN DOE",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar2,
+        name: "Tom Alex",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar3,
+        name: "Johnny",
+      },
+      {
+        review:
+          "“In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstra”",
+        img: avatar4,
+        name: "ROBBIN",
+      },
+    ],
+  },
+  Hireme: {
+    title: "Hire Me",
+    subtitle: "FOR YOUR PROJECTS",
+    image1: Hireme_person,
+    image2: Hireme_person2,
+    para: "In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document elying on mean",
+    btnText: "Hire Me",
+  },
+  Contact: {
+    title: "Contact",
+    subtitle: "For further information",
+    social_media: [
+      {
+        text: "Raja Khandelwal",
+        icon: BsFillPersonFill,
+        link: "mailto:codeaprogram@gmail.com",
+      },
+      {
+        text: 9831020874,
+        icon: MdCall,
+        link: "mailto:codeaprogram@gmail.com",
+      },
+      {
+        text: "Ajay Shah",
+        icon: BsFillPersonFill,
+        link: "mailto:codeaprogram@gmail.com",
+      },
+      {
+        // text: "Raja Khandelwal",
+        text: 9883093710,
+        icon: MdCall,
+        link: "mailto:codeaprogram@gmail.com",
+      },
+      {
+        text: "Nikhil Ginoria",
+        // text: 9831020874,
+        icon: BsFillPersonFill,
+        link: "mailto:codeaprogram@gmail.com",
+      },
+      {
+        // text: "Raja Khandelwal",
+        text: 9836545930,
+        icon: MdCall,
+        link: "mailto:codeaprogram@gmail.com",
+      },
+      // {
+      //   text: "+91 1234 56778",
+      //   icon: MdCall,
+      //   link: "https://wa.me/1234567890",
+      // },
+      // {
+      //   text: "codeaprogram",
+      //   icon: BsInstagram,
+      //   link: "https://www.instagram.com/codeaprogram/",
+      // },
+    ],
+  },
+  Footer: {
+    text: "All © Copy Right Reserved 2022",
+  },
+};
---
+++ src/components/Projects.jsx
@@ -0,0 +1,66 @@
+import { content } from "../Content";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+import { Pagination } from "swiper";
+
+const Projects = () => {
+  const { Projects } = content;
+  return (
+    <section className="bg-bg_light_primary" id="projects">
+      <div className="md:container px-5 pt-14 min-h-screen flex flex-col justify-between">
+        <div>
+          <h2 className="title" data-aos="fade-down">
+            {Projects.title}
+          </h2>
+          <h4 className="subtitle" data-aos="fade-down">
+            {Projects.subtitle}
+          </h4>
+          <br />
+        </div>
+        <div className="flex items-center lg:flex-row flex-col-reverse gap-5">
+          {/* <img
+            src={Projects.image}
+            alt="..."
+            data-aos="fade-right"
+            className="max-w-[45vw] min-w-[22rem]"
+          /> */}
+          {/* <Swiper
+            pagination={{
+              clickable: true,
+            }}
+            data-aos="fade-left"
+            spaceBetween={20}
+            modules={[Pagination]}
+            className="rounded-3xl pb-16 max-w-xs drop-shadow-primary self-start"
+          >
+            {Projects.project_content.map((content, i) => (
+              <SwiperSlide
+                key={i}
+                className="bg-white rounded-3xl p-5 border-b-8 border-[#FAF9FD] h-fit"
+              >
+                <img src={content.image} alt="..." />
+                <div className="flex flex-col gap-1 mt-2">
+                  <h5 className="font-bold font-Poppins">{content.title}</h5>
+                  <button className="font-bold text-gray self-end">
+                    READ MORE
+                  </button>
+                </div>
+              </SwiperSlide>
+            ))}
+          </Swiper> */}
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3683.318683827073!2d88.41010547504095!3d22.604571731798192!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a0275f0ea7cc93b%3A0xe76b66131336bca5!2sD%20Block%20Ground!5e0!3m2!1sen!2sin!4v1690706693592!5m2!1sen!2sin" 
+                      width="100%" height="600px" frameborder="0" 
+                      // style={{border:0}}
+                       ></iframe>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Projects;
